4a0e7adebf930d740943220722a8e1078321cf8d
sam-100 <76462898+sam-100@users.noreply.github.com>
2023-02-19 21:42:08 +0530

    Update maze.cpp

--- maze.cpp
+++ maze.cpp
@@ -41,6 +41,17 @@ void display(vector<vector<char>> const &arr) {
     }
     cout<<endl;
 }
+void display(vector<vector<int>> const &arr) { 
+    int n=arr.size();
+    cout<<"Game:"<<endl;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
 
 void display_path(vector<vector<char>> const &game, vector<vector<bool>> const &visited) {
     int n=game.size();
@@ -69,20 +80,20 @@ void editInstructions() {
     cout<<"\t8. 0: Exit 'Edit mode'"<<endl;
 }
 
-void clear(vector<vector<bool>> &arr) {
+void clear(vector<vector<bool>> &arr, bool val=false) {
     int n=arr.size();
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            arr[i][j]=false;
+            arr[i][j]=val;
         }
     }
 }
 
-void clear(vector<vector<int>> &arr) {
+void clear(vector<vector<int>> &arr, int val) {
     int n=arr.size();
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            arr[i][j]=5;
+            arr[i][j]=val;
         }
     }
 }
@@ -137,16 +148,17 @@ int trace_path(vector<vector<char>> const &arr, int r, int c, vector<vector<bool
     return cost;
 }
 
-int dfs(vector<vector<char>> const &arr, int r, int c, vector<vector<bool>> &visited, bool &found, vector<vector<int>> &path) {
+int dfs(vector<vector<char>> const &arr, int r, int c, vector<vector<bool>> &visited, vector<vector<int>> &total_cost, bool &found, vector<vector<int>> &path) {
     if(arr[r][c]==END) {
         found=true;
         return 0;
     }
     if(arr[r][c]==OBSTACLE) 
         return 0;
+    
     int cost=0;
-    visited[r][c]=true;
     int n=arr.size();
+    visited[r][c]=true;
 
     // Display progress 
         system("clear");
@@ -154,54 +166,82 @@ int dfs(vector<vector<char>> const &arr, int r, int c, vector<vector<bool>> &vis
         display_path(arr, visited);
         usleep(50000);
     
-    // north 
-        if(r-1>=0 && visited[r-1][c]==false && found==false) { // && arr[r-1][c]==BLANK) {
-            cost+=dfs(arr, r-1, c, visited, found, path);
-            visited[r-1][c]=true;
-            path[r-1][c]=2;
-        }
-    // north-east
-        if(r-1>=0 && c+1<n && visited[r-1][c+1]==false && found==false) {
-            cost+=dfs(arr, r-1, c+1, visited, found, path);
-            visited[r-1][c+1]=true;
-            path[r-1][c+1]=1;
-        }
-    // east
-        if(c+1<n && visited[r][c+1]==false && found==false) { // && arr[r][c+1]==BLANK) {
-            cost+=dfs(arr, r, c+1, visited, found, path);
-            visited[r][c+1]=true;
-            path[r][c+1]=4;
-        }
-    // south-east
-        if(r+1<n && c+1<n && visited[r+1][c+1]==false && found==false) {
-            cost+=dfs(arr, r+1, c+1, visited, found, path);
-            visited[r+1][c+1]=true;
-            path[r+1][c+1]=7;
-        }
-    // south 
-        if(r+1<n && visited[r+1][c]==false && found==false) { // && arr[r+1][c]==BLANK) {
-            cost+=dfs(arr, r+1, c, visited, found, path);
-            visited[r+1][c]=true;
-            path[r+1][c]=8;
-        }
-    // south-west
-        if(r+1<n && c-1>=0 && visited[r+1][c-1]==false && found==false) {
-            cost+=dfs(arr, r+1, c-1, visited, found, path);
-            visited[r+1][c-1]=true;
-            path[r+1][c-1]=9;
-        }
-    // west 
-        if(c-1>=0 && visited[r][c-1]==false && found==false) { // && arr[r][c-1]==BLANK) {
-            cost+=dfs(arr, r, c-1, visited, found, path);
-            visited[r][c-1]=true;
-            path[r][c-1]=6;
-        }
-    // north-west
-        if(r-1>=0 && c-1>=0 && visited[r-1][c-1]==false && found==false) {
-            cost+=dfs(arr, r-1, c-1, visited, found, path);
-            visited[r-1][c-1]=true;
-            path[r-1][c-1]=3;
-        }
+    // Updating all neighbours
+        // north 
+            if(r-1>=0 && found==false && total_cost[r-1][c]>total_cost[r][c]+10) { 
+                path[r-1][c]=2;
+                total_cost[r-1][c]=total_cost[r][c]+10;
+            }
+        // north-east
+            if(r-1>=0 && c+1<n && found==false && total_cost[r-1][c+1]>total_cost[r][c]+14) {
+                path[r-1][c+1]=1;
+                total_cost[r-1][c+1]=total_cost[r][c]+14;
+            }
+        // east
+            if(c+1<n && found==false && total_cost[r][c+1]>total_cost[r][c]+10) { 
+                path[r][c+1]=4;
+                total_cost[r][c+1]=total_cost[r][c]+10;
+            }
+        // south-east
+            if(r+1<n && c+1<n && found==false && total_cost[r+1][c+1]>total_cost[r][c]+14) {
+                path[r+1][c+1]=7;
+                total_cost[r+1][c+1]=total_cost[r][c]+14;
+            }
+        // south 
+            if(r+1<n && found==false && total_cost[r+1][c]>total_cost[r][c]+10) { 
+                path[r+1][c]=8;
+                total_cost[r+1][c]=total_cost[r][c]+10;
+            }
+        // south-west
+            if(r+1<n && c-1>=0 && found==false && total_cost[r+1][c-1]>total_cost[r][c]+14) {
+                path[r+1][c-1]=9;
+                total_cost[r+1][c-1]=total_cost[r][c]+14;
+            }
+        // west 
+            if(c-1>=0 && found==false && total_cost[r][c-1]>total_cost[r][c]+10) { 
+                path[r][c-1]=6;
+                total_cost[r][c-1]=total_cost[r][c]+10;
+            }
+        // north-west
+            if(r-1>=0 && c-1>=0 && found==false && total_cost[r-1][c-1]>total_cost[r][c]+14) {
+                path[r-1][c-1]=3;
+                total_cost[r-1][c-1]=total_cost[r][c]+14;
+            }
+
+    // Recursively exploring all the paths 
+        // north 
+            if(r-1>=0 && found==false && visited[r-1][c]==false) {
+                cost+=dfs(arr, r-1, c, visited, total_cost, found, path);
+            }
+        // north-east
+            if(r-1>=0 && c+1<n && found==false && visited[r-1][c+1]==false) {
+                cost+=dfs(arr, r-1, c+1, visited, total_cost, found, path);
+            }
+        // east
+            if(c+1<n && found==false && visited[r][c+1]==false) {
+                cost+=dfs(arr, r, c+1, visited, total_cost, found, path);
+            }
+        // south-east
+            if(r+1<n && c+1<n && found==false && visited[r+1][c+1]==false) {
+                cost+=dfs(arr, r+1, c+1, visited, total_cost, found, path);
+            }
+        // south 
+            if(r+1<n && found==false && visited[r+1][c]==false) {
+                cost+=dfs(arr, r+1, c, visited, total_cost, found, path);
+            }
+        // south-west
+            if(r+1<n && c-1>=0 && found==false && visited[r+1][c-1]==false) {
+                cost+=dfs(arr, r+1, c-1, visited, total_cost, found, path);
+            }
+        // west 
+            if(c-1>=0 && found==false && visited[r][c-1]==false) {
+                cost+=dfs(arr, r, c-1, visited, total_cost, found, path);
+            }
+        // north-west
+            if(r-1>=0 && c-1>=0 && found==false && visited[r-1][c-1]==false) {
+                cost+=dfs(arr, r-1, c-1, visited, total_cost, found, path);
+            }
+
     return cost+1;
 }
 
@@ -401,12 +441,14 @@ int main()
         bool found=false;
         vector<vector<bool>> visited(n);
         vector<vector<int>> path(n);
+        vector<vector<int>> total_cost(n);
         int difficulty=5;
         string status="NULL - [Chose an algorithm]";
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 visited[i].push_back(false);
                 path[i].push_back(5);
+                total_cost[i].push_back(INT32_MAX);
             }
         }
         
@@ -585,26 +627,27 @@ int main()
                                 break;
                             case 1:
                                 clear(visited);
-                                clear(path);
+                                clear(path, 5);
+                                clear(total_cost, INT32_MAX);
                                 found=false;
-                                search_cost=dfs(game, src.r, src.c, visited, found, path);
+                                search_cost=dfs(game, src.r, src.c, visited, total_cost, found, path);
                                 path_cost=trace_path(game, dst.r, dst.c, visited, path);
                                 break;
                             case 2:
                                 clear(visited);
-                                clear(path);
+                                clear(path, 5);
                                 search_cost=bfs(game, src, path);
                                 path_cost=trace_path(game, dst.r, dst.c, visited, path);
                                 break;
                             case 3:
                                 clear(visited);
-                                clear(path);
+                                clear(path, 5);
                                 search_cost=A_star(game, src, dst, path);
                                 path_cost=trace_path(game, dst.r, dst.c, visited, path);
                                 break;
                             case 4:
                                 clear(visited);
-                                clear(path);
+                                clear(path, 5);
                             default:
                                 break;
                         }
